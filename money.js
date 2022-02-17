0aa5ce005fbc01602a82fe3794a7280425fe7407
khalilsikder <khalil.sikder03@gmail.com>
2022-02-17 18:07:44 +0600

    code

--- money.js
+++ money.js
@@ -1,3 +1,14 @@
 document.getElementById("login-button").addEventListener('click',function(){
-    console.log('click');
+    let totalExpenses = document.getElementById('total-expenses');
+    let expenses = totalExpenses.value;
+    let inputFood = document.getElementById('input-food');
+    let Food = inputFood.value;
+    let inputRent = document.getElementById("input-rent");
+    let Rent = inputRent.value;
+    let inputClothes = document.getElementById("input-cloth");
+    let Clothes = inputClothes.value;
+    let sumExpenses =parseInt(Food) + parseInt(Rent) + parseInt(Clothes);
+    //  console.log(sumExpenses);
+    totalExpenses.value = sumExpenses
+    //  totalExpenses.value = '';
 })
